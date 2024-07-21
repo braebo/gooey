a8de37af836866e61c83f1f44aba0372a3990176
Braden Wiggins <braden@fractal-hq.com>
2024-07-21 19:35:07 -0400

    feat: temp redirects

--- www/src/routes/+layout.server.ts
+++ www/src/routes/+layout.server.ts
@@ -1,6 +1,12 @@
 import routes from '../../.svelte-kit/types/route_meta_data.json'
+import { redirect } from '@sveltejs/kit'
 
-export function load({ locals, depends }) {
+export function load({ locals, depends, url }) {
 	depends('routes')
+
+	// todo - remove these once we have a proper homepage
+	if (['/', '/demos'].includes(url.pathname)) {
+		redirect(307, '/demos/goo')
+	}
 	return { theme: locals.theme, routes: Object.keys(routes) as Array<keyof typeof routes> }
 }
