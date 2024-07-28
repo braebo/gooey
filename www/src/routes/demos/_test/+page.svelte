e9f06484d96fce8f49325df6398c563e76bdf10e
Braden Wiggins <braden@fractal-hq.com>
2024-07-27 20:45:49 -0400

    wip: test page

---
+++ www/src/routes/demos/_test/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { Gooey } from '../../../../../src/Gooey'
+
+	onMount(() => {
+		localStorage.clear()
+		const gooey = new Gooey({ title: 'test', themeMode: 'system', position: 'center' })
+
+		gooey.add('foo', 1)
+	})
+</script>
+
+<div>
+	<h1>Test Page</h1>
+</div>
+
+<style lang="scss">
+</style>
