01391c0de6217960d7d938b4f169e87738530f76
Braden Wiggins <braden@fractal-hq.com>
2024-08-08 19:50:15 -0400

    feat(www): isolate global layout styles for demos

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -17,21 +17,3 @@
 
 <!-- svelte-ignore slot_element_deprecated -->
 <slot />
-
-<style>
-	:global(body) {
-		overflow: hidden;
-		max-height: 100vh;
-	}
-	:global(:root[theme='light'] body) {
-		background-image: linear-gradient(75deg, var(--bw), var(--bg-c));
-	}
-
-	:global(:root[theme='dark'] body) {
-		background-image: linear-gradient(
-			24deg,
-			color-mix(in lab, var(--bw), var(--bg-a)) 0%,
-			color-mix(in lch, var(--bg-b) 20%, var(--bg-c) 45%)
-		);
-	}
-</style>
--- www/src/routes/demos/+layout.svelte
+++ www/src/routes/demos/+layout.svelte
@@ -1,1 +1,19 @@
 <slot />
+
+<style>
+	:global(body) {
+		/* overflow: hidden; */
+		min-height: 100vh;
+	}
+	:global(:root[theme='light'] body) {
+		background-image: linear-gradient(75deg, var(--bw), var(--bg-c));
+	}
+
+	:global(:root[theme='dark'] body) {
+		background-image: linear-gradient(
+			24deg,
+			color-mix(in lab, var(--bw), var(--bg-a)) 0%,
+			color-mix(in lch, var(--bg-b) 20%, var(--bg-c) 45%)
+		);
+	}
+</style>
