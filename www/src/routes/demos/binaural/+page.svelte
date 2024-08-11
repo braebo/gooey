f9b32bdac0aecb864635ab8f4043c8a4ec858cc9
Braden Wiggins <braden@fractal-hq.com>
2024-08-11 13:38:06 -0400

    fix(www): sync binaural demo

--- www/src/routes/demos/binaural/+page.svelte
+++ www/src/routes/demos/binaural/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import type { Gooey } from '../../../../../src'
+
+	import GooeyThemeSync from '$lib/components/GooeyThemeSync.svelte'
 	import { BinauralBeats } from './beats/BinauralBeats'
 	import { onMount } from 'svelte'
 
+	let gooey: Gooey
+
 	onMount(() => {
 		const beats = new BinauralBeats({ volume: 0.5 })
+		gooey = beats.gooey
 
 		return () => {
 			beats.dispose()
 		}
 	})
 </script>
+
+{#if gooey}
+	<GooeyThemeSync {gooey} />
+{/if}
