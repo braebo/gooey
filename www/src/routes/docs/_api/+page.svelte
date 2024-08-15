b770051881ac40368874df47b7abb5556633fea4
Braden Wiggins <braden@fractal-hq.com>
2024-08-14 22:14:11 -0400

    wip(www): generated api

---
+++ www/src/routes/docs/_api/+page.svelte
@@ -0,0 +1,13 @@
+<!-- title:API -->
+
+<script lang="ts">
+	import Doc from '$lib/components/doc/Doc.svelte'
+
+	export let data
+</script>
+
+<section>
+	{#each data.docs as doc}
+		<Doc {doc} />
+	{/each}
+</section>
