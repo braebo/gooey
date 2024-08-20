2f083d8f5b8fdbd7edf2ce7d237973326ca0c543
Braden Wiggins <braden@fractal-hq.com>
2024-08-20 18:12:54 -0400

    fix(www): vite allow list

--- www/vite.config.js
+++ www/vite.config.js
@@ -3,7 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-	},
+	server: { fs: { allow: ['..'] } },
 })
