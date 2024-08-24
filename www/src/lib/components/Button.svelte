9cb633d517f0909957b41ac82baca39bafaf8ee9
Braden Wiggins <braden@fractal-hq.com>
2024-08-24 18:49:38 -0400

    feat(www): who ain't got no buttons

---
+++ www/src/lib/components/Button.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	let {
+		children,
+		onclick = () => {},
+		center = false,
+		active = $bindable(false),
+	}: {
+		children: Snippet
+		onclick: () => void
+		center: boolean
+		active: boolean
+	} = $props()
+</script>
+
+<button {onclick} class:center class:active>
+	{@render children?.()}
+</button>
+
+<style lang="scss">
+	button {
+		background: var(--bg-b);
+		border: none;
+		outline: 1px solid color-mix(in lch, var(--bg-b), var(--bg-c) 50%);
+		border-radius: var(--radius-md);
+		width: fit-content;
+
+		padding: var(--padding-sm) var(--padding-md);
+		font-size: var(--font-xs);
+		font-family: var(--font-a);
+		font-variation-settings:
+			'wght' 400,
+			'wdth' 105;
+	}
+
+	.center {
+		display: flex;
+		margin: 0 auto;
+	}
+</style>
