96238599a98f54a0e50c70e3a39422950fda7e42
Braden Wiggins <braden@fractal-hq.com>
2024-08-28 20:32:14 -0400

    feat(www): template page

---
+++ www/src/routes/demos/_test/template/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Gooey } from '../../../../../../src/Gooey'
+	import { onMount } from 'svelte'
+
+	let params = $state({
+		foo: 1,
+		bar: 'baz',
+		qux: () => console.log('quux'),
+	})
+
+	onMount(() => {
+		const gooey = new Gooey({
+			title: 'Template',
+			themeMode: 'system',
+			position: 'center',
+		})
+
+		gooey.bindMany({})
+
+		return gooey.dispose
+	})
+</script>
