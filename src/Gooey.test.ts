da6956452a54754a6cd5af31dafd115c6ded15bb
Braden Wiggins <braden@fractal-hq.com>
2024-08-31 14:52:23 -0400

    feat: `Folder` tests

--- src/Gooey.test.ts
+++ src/Gooey.test.ts
@@ -7,7 +7,7 @@ const G = new GooeyTest()
 describe('Gooey constructor', () => {
 	let gooey: Gooey
 	test('empty init', () => {
-		gooey = new Gooey()
+		gooey = G.addGooey()
 	})
 
 	test('updating title and dispose', () => {
@@ -59,70 +59,8 @@ describe('width', () => {
 		expect(gooey.element.clientWidth).toBe(345)
 	})
 
-	test('100px', async () => {
+	test('100px correctly reduces minimum width', async () => {
 		const gooey = G.addGooey({ title: '100px', width: 100 })
 		expect(gooey.element.clientWidth).toBe(100)
-	})
-})
-
-describe('addMany', () => {
-	const stuff = {
-		switch: true,
-		number: 123,
-		string: 'foo',
-		nested: {
-			color: '#ff0000',
-			select: ['a', 'b', 'c'],
-		},
-	}
-
-	const gooey = G.addGooey({ height: 1000 })
-	gooey.addMany(stuff)
-
-	console.log('allInputs.keys()', [...gooey.allInputs.keys()])
-	console.log(
-		'children',
-		gooey.folder.children.map(c => c.title),
-	)
-
-	test('input switch', () => {
-		const inputSwitch = gooey.allInputs.get('switch')
-		expect(inputSwitch?.element, 'Failed to find input switch.').toBeDefined()
-		expect(inputSwitch?.__type, 'Generated wrong input A type.').toBe('InputSwitch')
-		expect(inputSwitch?.value, 'Bad input A value.').toBe(true)
-	})
-
-	test('input number', () => {
-		const inputNumber = gooey.allInputs.get('number')
-		expect(inputNumber?.element, 'Failed to find input number.').toBeDefined()
-		expect(inputNumber?.__type, 'Generated wrong input A type.').toBe('InputNumber')
-		expect(inputNumber?.value, 'Bad input A value.').toBe(123)
-	})
-
-	test('input string', () => {
-		const inputString = gooey.allInputs.get('string')
-		expect(inputString?.element, 'Failed to find input string.').toBeDefined()
-		expect(inputString?.__type, 'Generated wrong input A type.').toBe('InputText')
-		expect(inputString?.value, 'Bad input A value.').toBe('foo')
-	})
-
-	test('input nested folder', () => {
-		const inputNested = gooey.folder.children.find(c => c.title === 'nested')
-		expect(inputNested?.element, 'Failed to find input nested.').toBeDefined()
-		expect(inputNested?.__type, 'Generated wrong input A type').toBe('Folder')
-	})
-
-	test('input color', () => {
-		const inputColor = gooey.allInputs.get('color')
-		expect(inputColor?.element, 'Failed to find input color.').toBeDefined()
-		expect(inputColor?.__type, 'Generated wrong input A type.').toBe('InputColor')
-		expect(inputColor?.value, 'Bad input A value.').toBe('#ff0000')
-	})
-
-	test('input select', () => {
-		const inputSelect = gooey.allInputs.get('select')
-		expect(inputSelect?.element, 'Failed to find input select.').toBeDefined()
-		expect(inputSelect?.__type, 'Generated wrong input A type.').toBe('InputSelect')
-		expect(inputSelect?.value, 'Bad input A value.').toBe('a')
 	})
 })
