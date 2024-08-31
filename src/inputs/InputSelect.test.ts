80ce5528e42d20ebc54bb0749913ee5ad47f4ed9
Braden Wiggins <braden@fractal-hq.com>
2024-08-31 14:52:44 -0400

    feat: more robust tests for `InputSelect`

--- src/inputs/InputSelect.test.ts
+++ src/inputs/InputSelect.test.ts
@@ -1,7 +1,7 @@
+import { GooeyTest } from '../tests/Gooey/GooeyTest'
 import { test, expect, describe } from 'vitest'
-import { Gooey } from '../Gooey'
 
-document.body.style.background = 'black'
+const g = new GooeyTest()
 
 const options = [
 	{ label: 'foo', value: 'foo' },
@@ -13,7 +13,7 @@ test('InputSelect', () => {
 })
 
 describe('addSelect', () => {
-	const gui = new Gooey()
+	const gui = g.addGooey()
 
 	test('correctly adds the title', () => {
 		const select = gui.addSelect('testing 123', options)
@@ -47,5 +47,53 @@ describe('addSelect', () => {
 
 		expect(select.options).toStrictEqual(options)
 		expect(select.state.value).toStrictEqual(options[1])
+	})
+})
+
+describe('add', () => {
+	const gui = g.addGooey()
+
+	test('from labeled option', () => {
+		const input = gui.add('title', 'foo', { options: ['foo', 'bar', 'baz'] })
+		expect(input.__type).toStrictEqual('InputSelect')
+	})
+
+	test('from unlabeled string array', () => {
+		const input = gui.add('title', ['foo', 'bar', 'baz'])
+		expect(input.__type).toStrictEqual('InputSelect')
+	})
+})
+
+describe('addMany', () => {
+	const gui = g.addGooey()
+
+	test('from labeled option', () => {
+		gui.addMany(
+			{
+				foo: {
+					label: 'foo',
+					value: 'foo',
+				},
+			},
+			{
+				foo: {
+					__type: 'SelectInputOptions',
+					options: [
+						{ label: 'foo', value: 'foo' },
+						{ label: 'bar', value: 'bar' },
+						{ label: 'baz', value: 'baz' },
+					],
+				},
+			},
+		)
+
+		const input = gui.inputs.get('foo')!
+		expect(input.value).toMatchObject({ label: 'foo', value: 'foo' })
+	})
+
+	test('from unlabeled string array', () => {
+		gui.addMany({ foo: ['foo', 'bar', 'baz'] })
+		const input = gui.inputs.get('foo')!
+		expect(input.value).toMatchObject({ label: 'foo', value: 'foo' })
 	})
 })
