3a89a16a974aa6a4bbc10a0302e6a720261134d0
Braden Wiggins <braden@fractal-hq.com>
2024-09-02 20:03:43 -0400

    feat(www): update mobile nav

--- www/src/lib/components/Nav/Mobile/Mobile.svelte
+++ www/src/lib/components/Nav/Mobile/Mobile.svelte
@@ -2,7 +2,6 @@
 	import type { Branch } from '$lib/utils/tree'
 
 	import { isActive } from '$lib/utils/isActive'
-	// import PageFill from './PageFill.svelte'
 	import { fly } from 'svelte/transition'
 	import { page } from '$app/stores'
 
@@ -12,48 +11,45 @@
 <!-- <PageFill bind:showMenu /> -->
 
 <!-- {#if showMenu} -->
-	{#snippet subnav(link: Branch, i = 0, depth = 1)}
-		<ul>
-			{#each link.children?.filter((c) => !c.name.startsWith('_')) ?? [] as child, j (child.name)}
-				<div
-					class="li depth-{depth}"
-					class:active={isActive(child.name, $page.url.pathname)}
-					in:fly={{ y: -10 - 5 * i - j * 2.5 }}
-					style:view-transition-name="li-{child.name}"
-				>
-					<a data-sveltekit-prefetch href={child.path}>
-						{child.name}
-					</a>
-					{@render subnav(child, i, depth + 1)}
-				</div>
-			{/each}
-		</ul>
-	{/snippet}
+{#snippet subnav(link: Branch, i = 0, depth = 1)}
+	<ul>
+		{#each link.children?.filter(c => !c.name.startsWith('_')) ?? [] as child, j (child.name)}
+			<div
+				class="li depth-{depth}"
+				class:active={isActive(child.name, $page.url.pathname)}
+				in:fly={{ y: -10 - 5 * i - j * 2.5 }}
+				style:view-transition-name="li-{child.name}"
+			>
+				<a data-sveltekit-prefetch href={child.path}>
+					{child.name}
+				</a>
+				{@render subnav(child, i, depth + 1)}
+			</div>
+		{/each}
+	</ul>
+{/snippet}
 
-	<nav class="overlay" class:showMenu>
-		<ul>
-			{#each links ?? [] as link, i (link.name)}
-				<div class="li depth-0" in:fly={{ y: -10 - 5 * i }} style:view-transition-name="li-{link.name}">
-					<a data-sveltekit-prefetch href={link.path} class:active={isActive(link.name, $page.url.pathname)}>
-						{link.name}
-					</a>
-					{@render subnav(link, i)}
-				</div>
-			{/each}
-		</ul>
-	</nav>
+<nav class="overlay" class:showMenu>
+	<ul>
+		{#each links ?? [] as link, i (link.name)}
+			<div class="li depth-0" in:fly={{ y: -10 - 5 * i }} style:view-transition-name="li-{link.name}">
+				<a data-sveltekit-prefetch href={link.path} class:active={isActive(link.name, $page.url.pathname)}>
+					{link.name}
+				</a>
+				{@render subnav(link, i)}
+			</div>
+		{/each}
+	</ul>
+</nav>
+
 <!-- {/if} -->
 
 <style lang="scss">
-	@mixin flex-center {
+	nav.overlay {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-	}
-
-	nav.overlay {
-		@include flex-center;
 
 		view-transition-name: overlay;
 
@@ -67,9 +63,9 @@
 		opacity: 0;
 		background-color: rgba(0, 0, 0, 0.5);
 		backdrop-filter: blur(0);
-		
+
 		z-index: 100;
-		transition: 0.3s cubic-bezier(0.23, 1, 0.320, 1);
+		transition: 0.3s cubic-bezier(0.23, 1, 0.32, 1);
 		user-select: none;
 		pointer-events: none;
 
@@ -81,7 +77,6 @@
 	nav.overlay.showMenu {
 		opacity: 1;
 		backdrop-filter: blur(20px);
-		// transform: translate(0, 0);
 		transform: translate3d(0, 0, 0);
 		pointer-events: auto;
 
@@ -91,12 +86,14 @@
 	}
 
 	ul {
-		@include flex-center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 
 		margin: auto;
 		padding: 0;
 
-		// outline: 1px solid orange;
 		width: 100%;
 	}
 
@@ -106,30 +103,28 @@
 	.li {
 		width: 100%;
 
-		// outline: 1px solid green;
-		
-		transition: 1s cubic-bezier(0.23, 1, 0.320, 1);
+		animation: slideIn 1s cubic-bezier(0.23, 1, 0.32, 1) forwards;
+
+		opacity: 0;
 		transform: translate(0, 10%);
-		opacity: 1;
-
-		@starting-style {
-			opacity: 0;
+	}
+	@keyframes slideIn {
+		to {
 			transform: translate(0, 10%);
+			opacity: 1;
+			color: red !important;
 		}
-
-		> a {
-			font-family: var(--font-a);
-			font-size: 1.2rem;
-			font-variation-settings:
-				'wght' 400,
-				'wdth' 110;
-			text-transform: capitalize;
-		}
+	}
+	.li > a {
+		font-family: var(--font-a);
+		font-size: 1.2rem;
+		font-variation-settings:
+			'wght' 400,
+			'wdth' 110;
+		text-transform: capitalize;
 	}
 
 	.li.depth-0 {
-		// outline: 1px solid red;
-
 		line-height: 3;
 
 		> a {
@@ -143,9 +138,6 @@
 	}
 
 	a {
-		// outline: 1px solid blue;
-		// outline-offset: -4px;
-
 		display: flex;
 
 		justify-content: center;
@@ -158,10 +150,6 @@
 		pointer-events: all;
 
 		opacity: 0;
-
-		@starting-style {
-			opacity: 0;
-		}
 	}
 
 	nav.overlay.showMenu a {
