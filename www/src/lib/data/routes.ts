7dfb87edcf9f84d708be5d0890b8713b2b378f81
Braden Wiggins <braden@fractal-hq.com>
2024-09-07 19:09:15 -0400

    feat(www): update routes

--- www/src/lib/data/routes.ts
+++ www/src/lib/data/routes.ts
@@ -1,62 +1,64 @@
-interface Branch {
+export interface Branch {
 	name: string
 	path: string
 	children?: Branch[]
 }
 
-export const routes = {
-	name: 'home',
-	path: '/',
-	children: [
-		{
-			name: 'docs',
-			path: '/docs',
-			children: [
-				{
-					name: 'install',
-					path: '/docs#install',
-				},
-				{
-					name: 'api',
-					path: '/docs/_api',
-				},
-			],
-		},
-		{
-			name: 'demos',
-			path: '/demos',
-			children: [
-				{
-					name: 'binaural',
-					path: '/demos/binaural',
-				},
-				{
-					name: 'goo',
-					path: '/demos/goo',
-				},
-				{
-					name: 'orbs',
-					path: '/demos/orbs',
-				},
-				{
-					name: 'test',
-					path: '/demos/_test',
-					children: [
-						{
-							name: 'add many',
-							path: '/demos/_test/addMany',
-						},
-						{
-							name: 'input button grid',
-							path: '/demos/_test/inputbuttongrid',
-						},
-						{
-							name: 'preset manager',
-							path: '/demos/_test/presetmanager',
-						},
-					],
-				},
-			],
-		},
-	],
-}
+export const routes: Branch[] = [
+	{
+		name: 'home',
+		path: '/',
+		children: [
+			{
+				name: 'docs',
+				path: '/docs',
+				children: [
+					{
+						name: 'install',
+						path: '/docs#install',
+					},
+					{
+						name: 'api',
+						path: '/docs/_api',
+					},
+				],
+			},
+			{
+				name: 'demos',
+				path: '/demos',
+				children: [
+					{
+						name: 'binaural',
+						path: '/demos/binaural',
+					},
+					{
+						name: 'goo',
+						path: '/demos/goo',
+					},
+					{
+						name: 'orbs',
+						path: '/demos/orbs',
+					},
+					{
+						name: 'test',
+						path: '/demos/_test',
+						children: [
+							{
+								name: 'add many',
+								path: '/demos/_test/addMany',
+							},
+							{
+								name: 'input button grid',
+								path: '/demos/_test/inputbuttongrid',
+							},
+							{
+								name: 'preset manager',
+								path: '/demos/_test/presetmanager',
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+]
