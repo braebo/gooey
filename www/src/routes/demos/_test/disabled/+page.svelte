c9775314ad9c6f896b1f9d825d09dc8463de1440
Braden Wiggins <braden@fractal-hq.com>
2024-09-22 23:29:41 -0400

    feat(www): more gooey theme syncs

--- www/src/routes/demos/_test/disabled/+page.svelte
+++ www/src/routes/demos/_test/disabled/+page.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
+	import GooeyThemeSync from '$lib/components/GooeyThemeSync.svelte'
 	import { kitchen } from '../../../../../../src/demo/kitchen'
 	import { Gooey } from '../../../../../../src/Gooey'
 	import { onMount } from 'svelte'
 
-	onMount(() => {
-		const sink = kitchen({
-			title: 'Kitchen Sink',
-			themeMode: 'system',
-			position: 'top-center',
-		})
+	let gooey = $state<Gooey | undefined>(undefined)
+	let sink = $state<Gooey | undefined>(undefined)
 
-		const g = new Gooey({
+	onMount(() => {
+		gooey = new Gooey({
 			title: 'Disabler',
 			themeMode: 'system',
 			position: 'top-center',
 			offset: { y: 400 },
 		})
-
+		sink = kitchen({
+			title: 'Kitchen Sink',
+			themeMode: 'system',
+			position: 'top-center',
+		})
 		;['text', 'number', 'boolean', 'select', 'color', 'button'].forEach((inputName: string) => {
-			const input = sink.inputs.get(inputName)!
+			const input = sink!.inputs.get(inputName)!
 			if (Math.random() > 0.5) input.disable()
-			g.add(
+			gooey?.add(
 				inputName,
 				() => {
 					input.disabled = !input.disabled
@@ -33,8 +35,16 @@
 		})
 
 		return () => {
-			sink.dispose()
-			g.dispose()
+			sink?.dispose()
+			gooey?.dispose()
 		}
 	})
 </script>
+
+{#if gooey}
+	<GooeyThemeSync {gooey} />
+{/if}
+
+{#if sink}
+	<GooeyThemeSync gooey={sink} />
+{/if}
