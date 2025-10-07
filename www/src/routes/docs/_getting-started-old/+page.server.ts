bc25ee14bf1c8e57be4bc4b0a4e30e55257342d5
Braden Wiggins <braden@fractal-hq.com>
2025-10-07 19:10:45 -0400

    wip(www): old docs strat for posterity (will delete later)

---
+++ www/src/routes/docs/_getting-started-old/+page.server.ts
@@ -0,0 +1,42 @@
+import type { CodeToHastOptions } from 'shiki'
+
+import { data as import_ } from '$lib/data/docs/02_basics/Import.svelte'
+import { data as basics } from '$lib/data/docs/02_basics/Basics.svelte'
+
+import { transformerNotationHighlight, transformerNotationFocus, transformerNotationDiff } from '@shikijs/transformers'
+import { getHighlighterInstance } from '$lib/utils/highlight.svelte'
+
+export const prerender = true
+export const ssr = false
+
+/**
+ * todo - When this was in an external file, it refused to re-use the highlighter instance,
+ * todo - instead creating a new one each time... what was that about?
+ */
+const highlighter = await getHighlighterInstance()
+
+export async function load() {
+	const highlightedEntries = await Promise.all(
+		Object.entries(Object.assign({}, basics, import_)).map(async ([key, { code }]) => {
+			try {
+				const highlighted = highlighter.codeToHtml(code, {
+					lang: 'ts',
+					theme: 'serendipity',
+					transformers: [
+						transformerNotationHighlight(),
+						transformerNotationFocus(),
+						transformerNotationDiff(),
+					],
+				} as CodeToHastOptions<string, string>)
+				return [key, highlighted]
+			} catch (error) {
+				console.error(error)
+				return [key, code]
+			}
+		}),
+	)
+
+	return { highlighted: Object.fromEntries(highlightedEntries) }
+
+	return {}
+}
---
+++ www/src/routes/docs/_getting-started-old/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Basics from '$lib/data/docs/02_basics/Basics.svelte'
+
+	import InstallCommands from '$lib/data/docs/01_install/InstallCommands.svelte'
+	import Import from '$lib/data/docs/02_basics/Import.svelte'
+	// import Hero from '$lib/data/docs/01_install/Hero.svelte'
+</script>
+
+<template lang="pug">
+
+	h1 Getting Started
+
+	.br-lg
+
+	InstallCommands
+
+	.br-lg
+
+	Import
+
+	.br-lg
+
+	Basics
+
+	.br-xl
+
+</template>
